434d17a3d153fe7965ead401559019f2c6fbd6d5
end13113 <37712800+end13113@users.noreply.github.com>
2018-06-06 11:34:56 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,32 @@
 
+$(window).on('load', function() {
+  var subscribers         = 0,
+      views               = 0,
+      $subscriber_counter = $('#subscriber-counter');
+
+  function get_user_views_subscribers() {
+    var api_key = "AIzaSyBYmVleOuz4Hffe2x2xRdqBWLOjB52MEVE";
+    var channel_id = "UCskG7xk7hK7n8wSZsblz2vQ";
+    var data_url = "https://www.googleapis.com/youtube/v3/channels?part=statistics&id=" + channel_id + "&key=" + api_key;
+
+    $.ajax({
+        url : data_url,
+        type : 'GET',
+        success : function (json) {
+          var statistics = json.items[0].statistics;
+          subscribers = statistics.subscriberCount;
+
+          var subscriberString = subscribers.toString(subscribers);
+
+          subscriber_string = '';
+          for (i=0; i<subscriberString.length; i++) {
+            subscriber_string += '<div class="time flip"><span class="count top">' + subscriberString[i] + '</span></div>';
+          }
+
+          $subscriber_counter.html(subscriber_string);
+        }
+    });
+  }
+  get_user_views_subscribers()
+  setInterval(get_user_views_subscribers, 5000);
+});
